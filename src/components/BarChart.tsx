c937c36a91da83e6202953e006e604323e6c81c1
Maique Rosa <maique.rosa@gmail.com>
2023-05-18 18:49:32 -0300

    refact: change the useeffect from barchar to update when data change

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -70,7 +70,7 @@ function BarChart({ data }: BarchartProps) {
       maintainAspectRatio: false,
       responsive: true,
     });
-  }, []);
+  }, [data]);
   return (
     <>
       <div className="w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white  overflow-y-scroll">
