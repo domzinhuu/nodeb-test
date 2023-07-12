dfa1666cc60ec426ed00f48dd68133011918e2d9
Domzinhuu <maique.rosa@gmail.com>
2023-07-12 16:34:21 -0300

    fix: resolvendo bugs de login e de filtro

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,13 +3,13 @@ import { api } from "@/lib/axios";
 
 async function login(userName: string, password: string): Promise<any | null> {
   const response = await api.post(`/login`, { email: userName, password });
-  if (!response.headers.authorization) {
+  if (!response.data.accessToken) {
     return null;
   }
 
   const user: UserData = response.data.currentClient;
 
-  return { user, token: response.headers.authorization };
+  return { user, token: response.data.accessToken, refreshToken: response.data.refreshToken };
 }
 
 export const AuthService = {
